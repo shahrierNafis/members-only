1b60463571e4e098bb212e4e1d9e83b132e77087
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2023-10-21 12:08:39 +0600

    Refactor: home page to show massages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const Message = require("../models/Message");
+const asyncHandler = require("express-async-handler");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get(
+  "/",
+  asyncHandler(async function (req, res, next) {
+    const messages = await Message.find().populate("author").exec();
+    res.render("index", { messages });
+  })
+);
 
 module.exports = router;
